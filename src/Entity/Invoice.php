d5a079819ed980589e00dbb918d44500961bbb5b
sey_9 <sey_91360@hotmail.fr>
2019-10-14 15:17:05 +0200

    Adding groups of serialization and calculate resources

--- src/Entity/Invoice.php
+++ src/Entity/Invoice.php
@@ -6,6 +6,7 @@ use ApiPlatform\Core\Annotation\ApiFilter;
 use ApiPlatform\Core\Bridge\Doctrine\Orm\Filter\OrderFilter;
 use ApiPlatform\Core\Annotation\ApiResource;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Serializer\Annotation\Groups;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\InvoiceRepository")
@@ -14,7 +15,8 @@ use Doctrine\ORM\Mapping as ORM;
  *     "pagination_enabled"=true,
  *     "pagination_items_per_page"=20,
  *     "order":{"sentAt":"desc"}
- *     }
+ *     },
+ *     normalizationContext={"groups"={"invoices_read"}}
  * )
  * @ApiFilter(OrderFilter::class,
  *     properties={"amount","sentAt"}
@@ -26,34 +28,49 @@ class Invoice
      * @ORM\Id()
      * @ORM\GeneratedValue()
      * @ORM\Column(type="integer")
+     * @Groups({"invoices_read","customers_read"})
      */
     private $id;
 
     /**
      * @ORM\Column(type="float")
+     * @Groups({"invoices_read","customers_read"})
      */
     private $amount;
 
     /**
      * @ORM\Column(type="datetime")
+     * @Groups({"invoices_read","customers_read"})
      */
     private $sentAt;
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Groups({"invoices_read","customers_read"})
      */
     private $status;
 
     /**
      * @ORM\ManyToOne(targetEntity="App\Entity\Customer", inversedBy="invoices")
      * @ORM\JoinColumn(nullable=false)
+     * @Groups({"invoices_read"})
      */
     private $customer;
 
     /**
      * @ORM\Column(type="integer")
+     * @Groups({"invoices_read","customers_read"})
      */
     private $chrono;
+
+    /**
+     * Get the User who own this invoice
+     * @Groups({"invoices_read"})
+     * @return User
+     */
+    public function getUser(): User{
+        return $this->customer->getUser();
+    }
 
     public function getId(): ?int
     {
